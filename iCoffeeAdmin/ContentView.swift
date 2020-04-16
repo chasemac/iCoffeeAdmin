a937857355a9dddac70b42c6173ff21e9d89819f
Chase McElroy <ichasem@gmail.com>
2020-04-15 21:45:10 -0700

    recreated all the files because of an Xcode error created home screen and connected it to firebase

--- iCoffeeAdmin/ContentView.swift
+++ iCoffeeAdmin/ContentView.swift
@@ -9,8 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var orderListener = OrderListener()
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            List {
+                Section(header: Text("Active Orders")) {
+                    
+                    ForEach(self.orderListener.activeOrders ?? []) { order in
+                        
+                        HStack {
+                            Text(order.customerName)
+                            Spacer()
+                            Text("$\(order.amount.clean)")
+                        } // end of hstack
+                        
+                    } // end of foreach
+                }
+                // End of section 1
+                Section(header: Text("Completed Orders")) {
+                    
+                    ForEach(self.orderListener.completedOrders ?? []) { order in
+                        
+                        HStack {
+                            Text(order.customerName)
+                            Spacer()
+                            Text("$\(order.amount.clean)")
+                        } // end of hstack
+                        
+                    } // end of foreach
+                }
+                // End of section 2
+            } // end of list
+            .navigationBarTitle("Orders")
+        } // end of nav view
     }
 }
 
