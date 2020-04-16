5d1d396c6e81b5908a86251128ee25b363c6a898
Chase McElroy <ichasem@gmail.com>
2020-04-15 18:37:51 -0700

    Initial Commit

---
+++ iCoffeeAdmin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iCoffeeAdmin
+//
+//  Created by Chase McElroy on 4/15/20.
+//  Copyright © 2020 ChaseMcElroy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
